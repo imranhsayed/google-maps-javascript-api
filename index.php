5e4c1891540d3af446716474e3df0dbc7faf0e06
Imran Sayed <imranhsayed@gmail.com>
2018-02-20 00:05:40 +0530

    Add all files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Google Maps JavaScript API</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<label for="">Address: <input id="map-search" class="controls" type="text" placeholder="Search Box" size="104"></label><br>
+<label for="">Lat: <input type="text" class="latitude"></label>
+<label for="">Long: <input type="text" class="longitude"></label>
+
+<div id="map-canvas"></div>
+
+<script src="javascript.js"></script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB4ZF2GU_edxPVaVnbxcwuEvyyQhU21sFw&libraries=places&callback=initialize"></script>
+</body>
+</html>
---
+++ javascript.js
@@ -0,0 +1,128 @@
+function initialize() {
+
+	var mapOptions, map, marker, searchBox,
+		infoWindow = '';
+		addressEl = document.querySelector( '#map-search' ),
+		latEl = document.querySelector( '.latitude' ),
+		longEl = document.querySelector( '.longitude' ),
+		element = document.getElementById( 'map-canvas' );
+
+	mapOptions = {
+		// How far the maps zooms in.
+		zoom: 8,
+		// Current Lat and Long position of the pin/
+		center: new google.maps.LatLng( -34.397, 150.644 ),
+		// center : {
+		// 	lat: -34.397,
+		// 	lng: 150.644
+		// },
+		disableDefaultUI: false, // Disables the controls like zoom control on the map if set to true
+		scrollWheel: true, // If set to false disables the scrolling on the map.
+		draggable: true, // If set to false , you cannot move the map around.
+		// mapTypeId: google.maps.MapTypeId.HYBRID, // If set to HYBRID its between sat and ROADMAP, Can be set to SATELLITE as well.
+		// maxZoom: 11, // Wont allow you to zoom more than this
+		// minZoom: 9  // Wont allow you to go more up.
+
+	};
+
+	/**
+	 * Creates the map using google function google.maps.Map() by passing the id of canvas and
+	 * mapOptions object that we just created above as its parameters.
+	 *
+	 */
+	// Create an object map with the constructor function Map()
+	map = new google.maps.Map( element, mapOptions ); // Till this like of code it loads up the map.
+
+	/**
+	 * Creates the marker on the map
+	 *
+	 */
+	marker = new google.maps.Marker({
+		position: mapOptions.center,
+		map: map,
+		// icon: 'http://pngimages.net/sites/default/files/google-maps-png-image-70164.png',
+		draggable: true
+	});
+
+	/**
+	 * Creates a search box
+	 */
+	searchBox = new google.maps.places.SearchBox( addressEl );
+
+	/**
+	 * When the place is changed on search box, it takes the marker to the searched location.
+	 */
+	google.maps.event.addListener( searchBox, 'places_changed', function () {
+		var places = searchBox.getPlaces(),
+			bounds = new google.maps.LatLngBounds(),
+			i, place, lat, long,
+			addresss = places[0].formatted_address;
+
+		for( i = 0; place = places[i]; i++ ) {
+			bounds.extend( place.geometry.location );
+			marker.setPosition( place.geometry.location );  // Set marker position new.
+		}
+
+		map.fitBounds( bounds );  // Fit to the bound
+		map.setZoom( 15 ); // This function sets the zoom to 15, meaning zooms to level 15.
+		// console.log( map.getZoom() );
+		lat = marker.getPosition().lat();
+		long = marker.getPosition().lng();
+		latEl.value = lat;
+		longEl.value = long;
+
+		// Closes the previous info window if it already exists
+		if ( infoWindow ) {
+			infoWindow.close();
+		}
+		/**
+		 * Creates the info Window at the top of the marker
+		 */
+		infoWindow = new google.maps.InfoWindow({
+			content: addresss
+		});
+
+		infoWindow.open( map, marker );
+	} );
+
+
+		/**
+	 * Finds the new position of the marker when the marker is dragged.
+	 */
+	google.maps.event.addListener( marker, "dragend", function ( event ) {
+		var lat, long, address;
+
+		console.log( 'i am dragged' );
+		lat = marker.getPosition().lat();
+		long = marker.getPosition().lng();
+
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode( { latLng: marker.getPosition() }, function ( result, status ) {
+			if ( 'OK' === status ) {  // This line can also be written like if ( status == google.maps.GeocoderStatus.OK ) {
+				address = result[0].formatted_address;
+				addressEl.value = address;
+				latEl.value = lat;
+				longEl.value = long;
+
+			} else {
+				console.log( 'Geocode was not successful for the following reason: ' + status );
+			}
+
+			// Closes the previous info window if it already exists
+			if ( infoWindow ) {
+				infoWindow.close();
+			}
+
+			/**
+			 * Creates the info Window at the top of the marker
+			 */
+			infoWindow = new google.maps.InfoWindow({
+				content: address
+			});
+
+			infoWindow.open( map, marker );
+		} );
+	});
+
+
+}
